fa6b790016c6d9bf756d913f38c30f7cafbcedde
Rafael Malcervelli <48256253+MalcerOne@users.noreply.github.com>
2021-10-02 18:43:38 -0300

    como pausar o tc?

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -23,6 +23,21 @@
 /************************************************************************/
 /* defines                                                              */
 /************************************************************************/
+/**
+ *  Informacoes para o RTC
+ *  poderia ser extraida do __DATE__ e __TIME__
+ *  ou ser atualizado pelo PC.
+ */
+typedef struct  {
+  uint32_t year;
+  uint32_t month;
+  uint32_t day;
+  uint32_t week;
+  uint32_t hour;
+  uint32_t minute;
+  uint32_t second;
+} calendar;
+
 // LED1
 #define LED1_PIO      PIOA
 #define LED1_PIO_ID   ID_PIOA
@@ -65,12 +80,13 @@
 /************************************************************************/
 /* variaveis globais                                                    */
 /************************************************************************/
-volatile char but1_flag;
-volatile char but2_flag;
-volatile char but3_flag;
+volatile Bool but1_flag = false;
+volatile Bool but2_flag = false;
+volatile Bool but3_flag = false;
 volatile char flag_tc0 = 0;
 volatile char flag_tc1 = 0;
 volatile char flag_tc2 = 0;
+volatile char flag_rtc = 0;
 
 /************************************************************************/
 /* prototype                                                            */
@@ -83,6 +99,7 @@ void but2_callback(void);
 void but3_callback(void);
 void io_init(void);
 void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
+void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type);
 
 /************************************************************************/
 /* Handlers                                                             */
@@ -132,27 +149,52 @@ void TC2_Handler(void){
 	flag_tc2 = 1;
 }
 
+/**
+* \brief Interrupt handler for the RTC. Refresh the display.
+*/
+void RTC_Handler(void)
+{
+	uint32_t ul_status = rtc_get_status(RTC);
+
+	/* seccond tick	*/
+	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
+		
+	}
+	
+	/* Time or date alarm */
+	if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
+		flag_rtc = 1;
+	}
+	
+	rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+	rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+	rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
+	rtc_clear_status(RTC, RTC_SCCR_CALCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
+}
+
 /************************************************************************/
 /* fun??es                                                              */
 /************************************************************************/
 void but1_callback(void){
 	if (pio_get(BUT1_PIO, PIO_INPUT, BUT1_IDX_MASK)) {
 		// PINO == 1 --> Borda de subida
-		but1_flag = 1;
+		but1_flag = !but1_flag;
 	}
 }
 
 void but2_callback(void){
 	if (!pio_get(BUT2_PIO, PIO_INPUT, BUT2_IDX_MASK)) {
 		// PINO == 0 --> Borda de descida
-		but2_flag = 1;
+		but2_flag = !but2_flag;
 	}
 }
 
 void but3_callback(void){
 	if (pio_get(BUT3_PIO, PIO_INPUT, BUT3_IDX_MASK)) {
 		// PINO == 1 --> Borda de subida
-		but3_flag = 1;
+		but3_flag = !but3_flag;
 	}
 }
 
@@ -249,6 +291,30 @@ void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
 	/* Inicializa o canal 0 do TC */
 	tc_start(TC, TC_CHANNEL);
 }
+/**
+* Configura o RTC para funcionar com interrupcao de alarme
+*/
+void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type){
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_RTC);
+
+	/* Default RTC configuration, 24-hour mode */
+	rtc_set_hour_mode(rtc, 0);
+
+	/* Configura data e hora manualmente */
+	rtc_set_date(rtc, t.year, t.month, t.day, t.week);
+	rtc_set_time(rtc, t.hour, t.minute, t.second);
+
+	/* Configure RTC interrupts */
+	NVIC_DisableIRQ(id_rtc);
+	NVIC_ClearPendingIRQ(id_rtc);
+	NVIC_SetPriority(id_rtc, 4);
+	NVIC_EnableIRQ(id_rtc);
+
+	/* Ativa interrupcao via alarme */
+	rtc_enable_interrupt(rtc,  irq_type);
+}
+
 
 void io_init(void){
 
@@ -362,6 +428,21 @@ int main (void)
 
 	/* Insert application code here, after the board has been initialized. */
 	while(1) {
+		if(but1_flag){
+			/** Configura RTC */
+			calendar rtc_initial = {2018, 3, 19, 12, 15, 45 ,1};
+			RTC_init(RTC, ID_RTC, rtc_initial, RTC_IER_ALREN);
+			
+			/* configura alarme do RTC */
+			rtc_set_date_alarm(RTC, 1, rtc_initial.month, 1, rtc_initial.day);
+			rtc_set_time_alarm(RTC, 1, rtc_initial.hour, 1, rtc_initial.minute, 1, rtc_initial.second + 20);
+		}
+		if(but2_flag){
+			
+		}
+		if(but3_flag){
+			
+		}
 		if(flag_tc0){
 			pisca_led1(1,10);
 			flag_tc0 = 0;
@@ -374,5 +455,9 @@ int main (void)
 			pisca_led3(1,10);
 			flag_tc2 = 0;
 		}
+		
+		// Entra em sleep mode
+		// Código 'trava' aqui até ser 'acordado'
+		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 	}
 }
